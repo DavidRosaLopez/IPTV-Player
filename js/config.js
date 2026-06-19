0d7efed482daf4ccc8b730275777e2f8813b101c
drosalop <davorsevilla@hotmail.com>
2026-06-19 13:52:51 +0200

    feat: add local configuration file and exclude it from version control

---
+++ js/config.js
@@ -0,0 +1,24 @@
+/**
+ * config.js — Configuration file
+ * DO NOT COMMIT THIS FILE TO VERSION CONTROL
+ */
+const Config = {
+  defaultLists: [
+    {
+      id: 'predef_1',
+      name: 'http://cf.futuremyprovt.com',
+      type: 'xtream',
+      server: 'http://cf.futuremyprovt.com',
+      user: 'f7f23dd33459',
+      pass: '604a8e6f2c'
+    },
+    {
+      id: 'predef_2',
+      name: 'http://cf.magicmagstreg.com',
+      type: 'xtream',
+      server: 'http://cf.magicmagstreg.com',
+      user: 'f7f23dd33459',
+      pass: '604a8e6f2c'
+    }
+  ]
+};
